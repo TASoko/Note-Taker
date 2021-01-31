722cea6b850f93f5ea21d42859d6fbbe3d369ba4
Thokozile Soko <thokozile.soko@yahoo.com>
2021-01-30 19:41:24 -0500

    Got the server.js file to work and it successfully consoles app listening on port 3000

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -1,9 +1,20 @@
-// * Create a basic server using Express.JS
+//  Create a basic server using Express.JS
 var express = require("express");
-var path = require("path");
+// var path = require("path");
 
 // Set up Express App
 var app = express ();
 
 // Sets an initial port. 
 var PORT = process.env.PORT || 3000
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// LISTENER
+// The below code "starts" the server
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
