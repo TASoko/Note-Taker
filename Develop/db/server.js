701b3e8d68bf95c6b4eba6444e0cd6b46cd37215
Thokozile Soko <thokozile.soko@yahoo.com>
2021-01-30 19:31:28 -0500

    installed all packages and added nodemon to help make things easier. Also created a .gitignore file.

---
+++ Develop/db/server.js
@@ -0,0 +1,9 @@
+// * Create a basic server using Express.JS
+var express = require("express");
+var path = require("path");
+
+// Set up Express App
+var app = express ();
+
+// Sets an initial port. 
+var PORT = process.env.PORT || 3000
