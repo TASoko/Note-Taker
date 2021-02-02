059106dacb7117a6411c22eb282d2c100e24cb62
Thokozile Soko <thokozile.soko@yahoo.com>
2021-02-02 18:05:54 -0500

    i added notes on the server.js file explaining each ocde.

--- Develop/server.js
+++ Develop/server.js
@@ -13,13 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
+// Sets up the routes js files that will run when node server.js is typed in the terminal.
 require("./routes/apiroutes.js")(app);
 require("./routes/htmlroutes.js")(app);
 
 // LISTENER
 // The below code "starts" the server
 app.listen(PORT, function() {
+  //This is to verfiy that the server actually ran and on what port.
     console.log("App listening on PORT: " + PORT);
   });
   
