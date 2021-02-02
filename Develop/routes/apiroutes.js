70886923c1a0da0e3020a6e245dd77e765bcb454
Thokozile Soko <thokozile.soko@yahoo.com>
2021-02-01 20:07:20 -0500

    Through over and hour and a half of debugging with my TA we were able to fix my api routes. I still have a lot of comments and possible refactoring to do.

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,23 +1,25 @@
 // REQUIRE fs TO WRITE TO FILE
-var fs = require("fs")
+var fs = require("fs");
 // ===============================================================================
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
-
-  let note = [];
+module.exports = function (app) {
+  // let note = [];
   // API GET Requests
   // ---------------------------------------------------------------------------
-  app.get("/api/notes", function(req, res) {
-    fs.readFile("./db/db.json", "utf8", (err, data)=> {
-      if (err) throw err; { 
-        let notes = JSON.parse(data);
-        res.json(notes);
-      } 
-    })
+  app.get("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (!data) {
+        return (data = []);
+      }
+      if (err) throw err;
+      let notes = JSON.parse(data);
+      console.log("Getting these notes:", notes);
+      res.json(notes);
+    });
   });
 
   // API POST Requests
@@ -26,34 +28,56 @@ module.exports = function(app) {
   // ...the JSON is pushed
   // ---------------------------------------------------------------------------
 
-  app.post("/api/notes", function(req, res) {
+  app.post("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      let notes = [];
+      if (err) throw err;
+      if (!data) {
+        console.log("No data!");
+        notes = [];
+      } else {
+        console.log("Retrieving data!");
+        notes = JSON.parse(data);
+      }
+      console.log("These are the notes", notes);
 
- let db = "./db/db.json";
+      const inputNote = {
+        id: Math.floor(Math.random() * 100),
+        title: req.body.title,
+        text: req.body.text,
+      };
+      console.log("The new note is", inputNote);
+      notes.push(inputNote);
 
-        const inputNote = [];
-        inputNote.id = Math.random() * 100;
-        inputNote.body = req.body
+      console.log("The new note has been added!");
 
-        // let id = db.push(req.body);
-        
-        fs.writeFileSync("./db/db.json", JSON.stringify(inputNote.body),() => {
-        let id = db.push(req.body);
-          res.json({...req.body,id:id})
-        });    
-
-        console.log(inputNote); 
-        
-        res.json(inputNote);
-
-    }
-   
-  );
+      fs.writeFileSync("./db/db.json", JSON.stringify(notes), "utf-8");
+      res.json(inputNote);
+    });
+  });
 
   // ---------------------------------------------------------------------------
   // Delete option
-  app.post("/api/notes/:id", function(req, res) {
-    const deleteNotes = data.filter(item => item.id != req.params.id);
-    data = deleteNotes;
-    return res.redirect('/');
+  app.delete("/api/notes/:id", function (req, res) {
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      console.log("Retrieving data to delete a note!");
+      let notes = JSON.parse(data);
+      console.log("These are the notes, one to be deleted", notes);
+
+      const filteredNotes = notes.filter(note => {
+        console.log(note.id)
+        console.log(req.params.id)
+      return note.id != req.params.id
+      });
+
+      console.log("Note deleted! The notes are now:", filteredNotes)
+
+      fs.writeFileSync("./db/db.json", JSON.stringify(filteredNotes), "utf-8");
+
+      res.sendStatus(200);
+
+    });
   });
 };
